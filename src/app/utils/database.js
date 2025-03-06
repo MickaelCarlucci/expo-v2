9bd643088d1131d0f9a75a2639c99f3df0cd103a
MickaelCarlucci <frewmike17@gmail.com>
2025-03-06 16:26:11 +0100

    add painting datamapper for api request

---
+++ src/app/utils/database.js
@@ -0,0 +1,10 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+export default pool;
---
+++ src/app/utils/datamappers/datamapper.paintings.js
@@ -0,0 +1,47 @@
+import client from "../database.js";
+
+export async function findAll() {
+  const query = {
+    text: `SELECT * FROM "paintings";`,
+  };
+  const result = await client.query(query);
+  return result.rows;
+}
+
+export async function findOne(id) {
+  const query = {
+    text: `SELECT * FROM "paintings" WHERE id = $1;`,
+    values: [id],
+  };
+  const result = await client.query(query);
+  return result.rows;
+}
+
+export async function createPainting(title, paintingUrl, description, price) {
+  const query = {
+    text: `INSERT INTO "paintings" (title, painting_url, description, price) VALUES ($1, $2, $3, $4) RETURNING *;`,
+    values: [title, paintingUrl, description, price],
+  };
+  const result = await client.query(query);
+  return result.rows[0];
+}
+
+export async function updatePainting(fields, index) {
+  const query = {
+    text: `UPDATE "paintings" SET ${fields.join(
+      ", "
+    )} WHERE id = $${index} RETURNING *;`,
+    values,
+  };
+  const result = await client.query(query);
+  result.rows[0];
+}
+
+export async function deletePainting(id) {
+  const query = {
+    text: `DELETE FROM "paintings" WHERE id = $1;`,
+    values: [id],
+  };
+  const result = await client.query(query);
+  return result.rows[0];
+}
