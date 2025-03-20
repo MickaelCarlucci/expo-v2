a127ed63a024e7541457d04a792c730134cba5cc
MickaelCarlucci <frewmike17@gmail.com>
2025-03-20 15:02:32 +0100

    initialise home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,23 @@ import Image from "next/image";
 import Link from "next/link";
 import "./page.css";
 
-export default function page() {
+export default function Page() {
+  const [paintings, setPaintings] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPaintings() {
+      try {
+        const response = await fetch("api/paintings");
+        const data = await response.json();
+        setPaintings(data);
+        setError(null);
+      } catch (error) {
+        setError("erreur lors de la récupération des tableaux");
+        console.error("erreur lors de la récupération des tableaux:", error);
+      }
+    }
+  });
+
   return <div></div>;
 }
